62243a5c1d0f48393135d4bb94ff1f518c96e992
DC <duckcuong@gmail.com>
2023-04-30 05:12:37 +0700

    Dung UI phan Header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 //bind object styles tra ra function cx de dung class viet classname
 //duoi dau gach ngang
 
+console.log(images.logo);
+
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="TikTok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search account and videos " spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
         </header>
     );
 }
